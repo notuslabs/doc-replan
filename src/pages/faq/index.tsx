a6135a1ff6f6c7a84b81d8607c73c973d3ee3c97
Bernardo Muller <bernardo.muller.bm@gmail.com>
2023-08-07 11:08:53 -0300

    feat: separate into three sections, add rightside external links, move a few contents (just a few)

---
+++ src/pages/faq/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "@theme/Layout"
+import React from "react"
+
+export default function Faq() {
+  return (
+    <Layout
+      // title={siteConfig.title}
+      description="FAQ do portal REPLAN Florianópolis"
+    >
+      <div>
+        <h1>Frequently Asked Questions</h1>
+        <ul>
+          <li>1- Radical</li>
+        </ul>
+      </div>
+    </Layout>
+  )
+}
